e85da3e3daac0c7ce4a2486a7214fdc0bdf7e1fe
Tajmir Khan <mf19-14@cs.qau.edu.pk>
2024-05-13 21:41:28 +0500

    Create self refrences using 'this' keyword.cpp

---
+++ self refrences using 'this' keyword.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class ABC {
+public:
+	ABC& f1() {
+		std::cout << "f1 function" << std::endl;
+		return *this;
+	}
+
+	ABC& f2() {
+		std::cout << "f2 function" << std::endl;
+		return *this;
+	}
+
+	ABC& f3() {
+		std::cout << "f3 function" << std::endl;
+		//std::cout << this << std::endl;
+		return *this;
+	}
+};
+
+
+int main() {
+	ABC obj;
+	obj.f1().f2().f3();
+	return 0;
+}
