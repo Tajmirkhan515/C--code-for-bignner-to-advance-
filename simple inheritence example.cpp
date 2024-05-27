f85c2c5fb16b5946ec3d1f6e34a57eb6be493ce7
Tajmir Khan <mf19-14@cs.qau.edu.pk>
2024-05-27 15:13:59 +0500

    derived class constructor

---
+++ simple inheritence example.cpp
@@ -0,0 +1,53 @@
+
+
+#include <iostream>
+using namespace std;
+
+////////////////////////////////////////////////////////////////
+class Counter //base class
+{
+protected: //NOTE: it can access in the child class
+	 int c; 
+public:
+	Counter()  //default
+	{ 
+		c = 0;
+	}
+	Counter(int a) //one argument
+	{
+		c = a;
+	}
+	
+	int get_count() const //print value of c
+	{
+		return c;
+	}
+	Counter operator ++ () //incrment, 
+	{
+		return Counter(++c);
+	}
+};
+
+
+// inherited from counter class
+class CountDn : public Counter 
+{
+public:
+	Counter operator -- () 
+	{
+		return Counter(--c);
+	}
+};
+
+
+int main()
+{
+	CountDn c1; 
+	cout << "\nc1 = " << c1.get_count(); 
+	++c1; ++c1; ++c1; 
+	cout << "\nc1 = " << c1.get_count(); 
+	--c1; --c1; 
+	cout << "\nc1 = " << c1.get_count(); 
+	cout << endl;
+	return 0;
+}
