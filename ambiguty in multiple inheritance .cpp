bb08d33a277cebe5a610e954987ed42f3c8ea0a8
Tajmirkhan515 <tajmirkhan515@gmail.com>
2024-06-04 10:25:43 +0500

    Create ambiguty in multiple inheritance .cpp

---
+++ ambiguty in multiple inheritance .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+	void show() { cout << "Class A\n"; }
+};
+class B
+{
+public:
+	void show() { cout << "Class B\n"; }
+};
+class C : public A, public B
+{
+};
+
+int main()
+{
+	C objC; 
+	//objC.show(); //ambiguous
+	objC.A::show(); 
+	objC.B::show(); 
+	return 0;
+}
